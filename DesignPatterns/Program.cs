1ec793803acb04f9745ba861c9722801540aa4a2
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-20 03:16:27 +0200

    initiall commit

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace DesignPatterns
+﻿using System;
+
+namespace SingletonPattern
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Singleton instance1 = Singleton.GetInstance();
+            Singleton instance2 = Singleton.GetInstance();
+
+            Console.WriteLine($"Instance 1 reference: {instance1.GetHashCode()}");
+            Console.WriteLine($"Instance 2 reference: {instance2.GetHashCode()}");
+            Console.WriteLine($"Are both instances the same object? {instance1 == instance2}");
+
+            instance1.PrintMessage();
         }
     }
 }
---
+++ Patterns1/Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object lockObject = new object();
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine("Hello! I am a Singleton instance.");
+        }
+    }
+}
